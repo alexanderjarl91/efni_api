637673d21113be5d43ac9614d393b3a18ac4533f
Johann <johannth88@gmail.com>
2021-02-01 10:25:41 +0000

    added star in noike

--- routes/nike.js
+++ routes/nike.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Nike = require('../models/Nike');
 
+router.get('*', function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+          // request was via https, so do no special handling
+          next();
+  } else {
+          // request was via http, so redirect to https
+          res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 // Get all products
 router.get('/', async (req, res) => {
   try {
