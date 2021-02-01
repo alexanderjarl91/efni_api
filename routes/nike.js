9f2c1c43b07b2e583cb472f3ac69f6345db9f0c0
Johann <johannth88@gmail.com>
2021-02-01 10:41:33 +0000

    another

--- routes/nike.js
+++ routes/nike.js
@@ -8,7 +8,7 @@ router.use('*', function (req, res, next) {
           next();
   } else {
           // request was via http, so redirect to https
-          res.redirect('https://' + req.headers.host + req.url + '/nike');
+          res.redirect('https://' + req.headers.host + req.url + 'nike');
   }
 });
 
