32308af55e29dfd299ad27161e1c7e067b4f2549
Johann <johannth88@gmail.com>
2021-02-01 10:37:21 +0000

    use instead of get

--- routes/nike.js
+++ routes/nike.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Nike = require('../models/Nike');
 
-router.get('*', function (req, res, next) {
+router.use('*', function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'https') {
           // request was via https, so do no special handling
           next();
