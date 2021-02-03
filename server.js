f53743e2c3fcdfed7bddd148dd12834263830a08
Johann <johannth88@gmail.com>
2021-02-03 17:22:28 +0000

    headers with cors stuff

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/nike", nikeRoute);
 app.use("/adidas", adidasRoute);
 const MONGO_URI = process.env.MONGODB_URI;
