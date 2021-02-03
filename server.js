c796e1bf323a892b45afe0d4162584bbc9a1bc2b
Johann <johannth88@gmail.com>
2021-02-03 17:03:02 +0000

    tmp cors removed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+//const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const nikeRoute = require('./routes/nike');
@@ -11,7 +11,7 @@ const Adidas = require('./models/Adidas');
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
