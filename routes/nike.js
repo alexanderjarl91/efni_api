a011d0169b36dba6e9de0ee65f879e35b34bbd2d
Johann <johannth88@gmail.com>
2021-02-03 16:42:06 +0000

    env variables

--- routes/nike.js
+++ routes/nike.js
@@ -6,8 +6,20 @@ var serviceAccount = require("../efni-cms-firebase-adminsdk-61zph-cebca0c788.jso
 const { firestore } = require('firebase-admin');
 
 // Initialize firebase 
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
 });
 
 // Initialize firestore
